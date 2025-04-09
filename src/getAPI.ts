976a8ee20092f0a7059be19765bf0367822c0c32
Рома Кушнірчук <groma1535@gmail.com>
2025-04-09 23:01:41 +0300

    Add buttons

--- src/getAPI.ts
+++ src/getAPI.ts
@@ -1,5 +1,16 @@
 const tbody = document.querySelector("tbody") as HTMLTableSectionElement;
-console.log(tbody);
+
+const btnPrice = document.querySelector(
+  ".btn-price"
+) as HTMLButtonElement | null;
+
+const btnPriceChange = document.querySelector(
+  ".btn-change"
+) as HTMLButtonElement | null;
+
+const btnPriceChangePercent = document.querySelector(
+  ".btn-change-percent"
+) as HTMLButtonElement | null;
 
 export interface Coin {
   map(arg0: (el: any, index: any) => void): unknown;
@@ -35,6 +46,15 @@ export async function getApi(): Promise<Coin[] | null> {
   }
 }
 
+const arrow: string = `
+<svg width="20" height="20" viewBox="0 0 20 20" fill="none"
+							xmlns="http://www.w3.org/2000/svg">
+							<path
+								d="m8.728 15.795-5-8A1.5 1.5 0 0 1 5 5.5h10a1.5 1.5 0 0 1 1.272 2.295l-5 8a1.5 1.5 0 0 1-2.544 0Z"
+								fill="#50e20c" />
+						</svg>
+`;
+
 function createDataTable(array: Coin[], elem: HTMLTableSectionElement) {
   let rowsHTML = ""; // змінна для накопичення HTML-рядків
   const newArray: Coin[] = array.slice(0, 10);
@@ -56,7 +76,11 @@ function createDataTable(array: Coin[], elem: HTMLTableSectionElement) {
 
   elem.innerHTML = rowsHTML; // вставляємо всі рядки за один раз
 }
-
 function slicedArray(array: Coin[]): Coin[] {
   return array.slice(0, 10);
+}
+function changePrice(array: Coin[]): Coin[] {
+  const newArr: Coin[] = [];
+
+  return newArr;
 }
